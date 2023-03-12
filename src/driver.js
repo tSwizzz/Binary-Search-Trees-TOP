9a34b95cef1f9798995160656a5d6199fc8668da
Eddy G <eddy.spambari@gmail.com>
2023-03-12 06:23:03 -0700

    added functions to eventually test everything later

--- src/driver.js
+++ src/driver.js
@@ -1,5 +1,19 @@
-//creates an array of random numbers from 1-100 and calls merge.js
-//to create a if it happens to not be sorted already
+import { Tree } from "./bst";
+import { Node } from "./bst";
+let tree;
 
+//sorts array and grabs root as well --> tree.sortedArr and tree.root
+export function arrayAndRoot(arr) {
+    tree = new Tree(arr);
+}
 
-//confirms that the tree is balanced
+//lets check it all!
+export function driver() {
+    tree.isBalanced(tree.root);
+    tree.insert(tree.root, 21);
+    tree.insert(tree.root, 25);
+    tree.insert(tree.root, 26);
+    tree.insert(tree.root, 78);
+    tree.insert(tree.root, 281);
+    tree.prettyPrint(tree.root);
+}
