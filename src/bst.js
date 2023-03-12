fd5a45d1da6c60b2e47a117011aa45c10eff0114
Eddy G <eddy.spambari@gmail.com>
2023-03-12 06:20:49 -0700

    added more functions for assignment

--- src/bst.js
+++ src/bst.js
@@ -1,4 +1,5 @@
-import { mergeSortRecursion, removeDuplicates } from "./merge";
+import mergeSortRecursion from "./merge.js"
+import { arrayAndRoot, driver} from "./driver.js";
 
 export class Node {
     constructor(data = null, left = null, right = null) {
@@ -24,29 +25,63 @@ export class Tree {
         root.right = this.buildTree(sortedArr, mid + 1, end);
 
         return root;
-
     }
 
     //accepts a value to insert
     insert(root, key) {
         if(root == null) {
-            return root = new Node(key);
-        }
+            root = new Node(key);
 
-        if(root.data < key) {
+            //after node is inserted, we re-balance the tree and get a new root
+            this.reBalanceInsert(key);
+        } else if(root.data < key) {
             root.right = this.insert(root.right, key);
-        } else {
+        } else if(root.data > key) {
             root.left = this.insert(root.left, key);
         }
-
+        if(root.data == key) {
+            return root;
+        }
         return root;
     }
 
     //accepts a value to delete
     delete(root, key) {
-        let copy;
+        if(root.data == key) {
+            if(root.left == null && root.right == null) {
+                root.data = null;
+            } else if(root.left == null && root.right != null) {
+                root.data = root.right.data;
+                root.right = null;
+            } else if(root.right == null && root.left != null) {
+                root.data = root.left.data;
+                root.left = null;
+            } else {
+                let rootToDelete = root;
+                root = root.right;
 
+                while(root.left) {
+                    root = root.left;
+                }
+                rootToDelete.data = root.data;
+                root.data = null;
+            }
+            
+            //after node is deleted, we re-balance the tree and get a new root
+            this.sortedArr = this.reBalanceDelete(this.sortedArr, key);
+            this.root = this.buildTree(this.sortedArr, 0, this.sortedArr.length - 1);
+        }
 
+        if(root.data != key) {
+            if(root.data < key && root.right != null) {
+                this.delete(root.right, key);
+            }
+            if(root.data > key && root.left != null) {
+                this.delete(root.left, key);
+            }
+        }
+
+        return;
     }
 
     //accepts a value and returns the node with the given value
@@ -60,22 +95,66 @@ export class Tree {
         } 
         return this.find(root.left, key);
     }
+
+    levelOrder() {
+        //do this next :)
+    }
+
+    //checks if tree is balanced
+    isBalanced(root) {
+        let originalRoot = root;
+        let lCount = 0;
+        let rCount = 0;
+
+        while(root.left) {
+            root = root.left;
+            lCount++;
+        }
+
+        root = originalRoot;
+
+        while(root.right) {
+            root = root.right;
+            rCount++;
+        }
+
+        if(lCount == rCount || lCount - rCount == -1 || lCount - rCount == 1) {
+            return console.log("The tree is balanced!");
+        }
+
+        return console.log("The tree is not balanced!");
+    }
+
+    reBalanceInsert(key) {
+        this.sortedArr.push(key);
+        this.sortedArr = mergeSortRecursion(this.sortedArr);
+        this.root = this.buildTree(this.sortedArr, 0, this.sortedArr.length - 1);
+    }
+
+    reBalanceDelete(arr, key) {
+        let newArr = [];
+        for(let k = 0; k < arr.length; k++) {
+            if(arr[k] != key) {
+                newArr.push(arr[k]);
+            }
+        }
+        return mergeSortRecursion(newArr);
+    }
+
+    //bst visual
+    prettyPrint(node, prefix = '', isLeft = true) {
+        if (node.right !== null) {
+        this.prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+        }
+        console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
+        if (node.left !== null) {
+        this.prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+        }
+    }
 }
+let array = [2,4,6,8,10,12,14];
+arrayAndRoot(array);
+driver();
 
-const tree = new Tree([1,2,3,4,5]);
-
-
-//bst visual
-const prettyPrint = (node, prefix = '', isLeft = true) => {
-    if (node.right !== null) {
-      prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
-    }
-    console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
-    if (node.left !== null) {
-      prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
-    }
-  }
-
-prettyPrint(tree.root);
 
 
